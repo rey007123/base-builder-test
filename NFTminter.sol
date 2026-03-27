e7b7a4b5807ad68f4c667f14c6c8b14f302d8f00
reyyan <66903937+rey007123@users.noreply.github.com>
2026-03-28 00:33:32 +0700

    Create NFTminter.sol

---
+++ NFTminter.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract NFTMinter {
+    uint256 public tokenId = 0;
+
+    function mint() public returns (uint256) {
+        tokenId += 1;
+        return tokenId;
+    }
+
+    function totalMinted() public view returns (uint256) {
+        return tokenId;
+    }
+}
