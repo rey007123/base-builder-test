d9635e1666e23cf86c05b5975d53fb604a32943c
reyyan <66903937+rey007123@users.noreply.github.com>
2026-03-28 00:33:59 +0700

    Create Lottery.sol

---
+++ Lottery.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Lottery {
+    address[] public participants;
+
+    function enter() public {
+        participants.push(msg.sender);
+    }
+
+    function getParticipantCount() public view returns (uint256) {
+        return participants.length;
+    }
+}
