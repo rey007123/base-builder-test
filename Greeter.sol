37b9fd4b6f908e63a5f5bb7eebe784860cefa4a7
reyyan <66903937+rey007123@users.noreply.github.com>
2026-03-28 00:41:04 +0700

    Update Greeter.sol

--- Greeter.sol
+++ Greeter.sol
@@ -1,1 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+contract HelloBase {
+    string public message = "Welcome to Base Builders!";
+
+    function setMessage(string memory newMessage) public {
+        message = newMessage;
+    }
+
+    function getMessage() public view returns (string memory) {
+        return message;
+    }
+}
