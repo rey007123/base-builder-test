0c80bcd3cd4bce34448e03b6f4a6eccf0938722c
reyyan <66903937+rey007123@users.noreply.github.com>
2026-03-28 00:39:27 +0700

    Update BasicMath.sol

--- BasicMath.sol
+++ BasicMath.sol
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
-
 contract BasicMath {
     function add(uint256 a, uint256 b) public pure returns (uint256) { return a + b; }
     function subtract(uint256 a, uint256 b) public pure returns (uint256) { return a - b; }
