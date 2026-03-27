265ac98fa811b5704b7684aa9421d3a398c678b4
reyyan <66903937+rey007123@users.noreply.github.com>
2026-03-28 00:36:00 +0700

    Create Fundraiser.sol

---
+++ Fundraiser.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Fundraiser {
+    uint256 public totalRaised;
+
+    function donate() public payable {
+        totalRaised += msg.value;
+    }
+
+    function getTotal() public view returns (uint256) {
+        return totalRaised;
+    }
+}
