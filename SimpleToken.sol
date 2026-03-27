db1923562f842eb121a07ddb24f79921f654194d
reyyan <66903937+rey007123@users.noreply.github.com>
2026-03-28 00:28:51 +0700

    Update SimpleToken.sol

--- SimpleToken.sol
+++ SimpleToken.sol
@@ -1,1 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+contract SimpleToken {
+    string public name = "BaseTestToken";
+    string public symbol = "BTT";
+    uint8 public decimals = 18;
+    uint256 public totalSupply;
+    mapping(address => uint256) public balanceOf;
+
+    constructor() { totalSupply = 1000000 * 10**18; balanceOf[msg.sender] = totalSupply; }
+
+    function transfer(address to, uint256 amount) public returns (bool) {
+        require(balanceOf[msg.sender] >= amount, "Not enough balance");
+        balanceOf[msg.sender] -= amount;
+        balanceOf[to] += amount;
+        return true;
+    }
+}
