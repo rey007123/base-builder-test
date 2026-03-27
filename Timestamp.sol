efa3ede6ed53d786d9b6d39a359677377e418a13
reyyan <66903937+rey007123@users.noreply.github.com>
2026-03-28 00:30:22 +0700

    Update Timestamp.sol

--- Timestamp.sol
+++ Timestamp.sol
@@ -1,1 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+contract Timestamp {
+    uint256 public deploymentTime;
+
+    constructor() { deploymentTime = block.timestamp; }
+
+    function getTimeSinceDeploy() public view returns (uint256) {
+        return block.timestamp - deploymentTime;
+    }
+}
