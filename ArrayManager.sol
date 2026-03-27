455ce98fcee598bcf84395a34ef9ee9bd9b7c664
reyyan <66903937+rey007123@users.noreply.github.com>
2026-03-28 00:37:14 +0700

    Create ArrayManager.sol

---
+++ ArrayManager.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract ArrayManager {
+    uint256[] public numbers;
+
+    function addNumber(uint256 num) public {
+        numbers.push(num);
+    }
+
+    function getLength() public view returns (uint256) {
+        return numbers.length;
+    }
+}
