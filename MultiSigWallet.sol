4404f34260d90ea3a3fabdcb58de922a3d4eae20
reyyan <66903937+rey007123@users.noreply.github.com>
2026-03-28 00:35:37 +0700

    Create MultiSigWallet.sol

---
+++ MultiSigWallet.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract MultiSigWallet {
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function confirmTransaction() public view returns (bool) {
+        return msg.sender == owner;
+    }
+}
