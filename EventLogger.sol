7a1268a4780aa4fdf04f4bcc0f85f9dce6518530
reyyan <66903937+rey007123@users.noreply.github.com>
2026-03-28 00:27:49 +0700

    Create EventLogger.sol

---
+++ EventLogger.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract EventLogger {
+    event LogMessage(string message, address sender);
+
+    function log(string memory message) public {
+        emit LogMessage(message, msg.sender);
+    }
+}
